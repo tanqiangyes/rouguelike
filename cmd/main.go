8615fc447df68611557d16d10d2936c657d28e6a
tanqiang <Tanqiang@enjoypartytime.com>
2024-12-25 12:13:27 +0800

    test fyne

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Rouguelike")
 
+	w.Resize(fyne.NewSize(800, 600))
+	w.SetMainMenu(fyne.NewMainMenu(
+		fyne.NewMenu("编辑"),
+	),
+	)
 	w.SetContent(widget.NewLabel("Hello World!"))
 	w.ShowAndRun()
 }
